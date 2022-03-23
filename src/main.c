87c2220a66adb946100c31e3bf0dae01d18dbde0
vinsdragonis <zrexteam128@gmail.com>
2022-03-23 14:14:16 +0530

    Created chip8 memory

--- src/main.c
+++ src/main.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include "SDL2/SDL.h"
+#include "chip8.h"
 
 int main(int argc, char **argv) {
+    struct chip8 chip8;
+    chip8_memory_set(&chip8.memory, 0x400, 'Z');
+    printf("%c\n", chip8_memory_get(&chip8.memory, 0x400));
+
     SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_Window* window = SDL_CreateWindow(
-        "Pixis",
+    SDL_Window *window = SDL_CreateWindow(
+        EMULATOR_WINDOW_TITLE,
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
-        640, 320, SDL_WINDOW_SHOWN
+        CHIP8_WIDTH * CHIP8_WINDOW_MULTIPLIER,
+        CHIP8_HEIGHT * CHIP8_WINDOW_MULTIPLIER,
+        SDL_WINDOW_SHOWN
     );
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
