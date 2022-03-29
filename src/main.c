a30e87793fe5fd9f2820e041865ebab67bd587c5
vinsdragonis <zrexteam128@gmail.com>
2022-03-29 18:19:37 +0530

    Added display library functions
    - Added library functions to display pixels on the screen
    - Fixed the default character set

--- src/main.c
+++ src/main.c
@@ -15,6 +15,9 @@ int main(int argc, char **argv) {
     struct chip8 chip8;
 
     chip8_init(&chip8);
+
+    chip8_screen_set(&chip8.screen, 15, 15);
+
     SDL_Init(SDL_INIT_EVERYTHING);
 
     SDL_Window *window = SDL_CreateWindow(
@@ -66,14 +69,20 @@ int main(int argc, char **argv) {
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
         SDL_RenderClear(renderer);
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
-        
-        SDL_Rect r;
-        r.x = 50;
-        r.y = 50;
-        r.w = 40;
-        r.h = 40;
 
-        SDL_RenderFillRect(renderer, &r);
+        for (int x = 0; x < CHIP8_WIDTH; x++) {
+            for (int y = 0; y < CHIP8_HEIGHT; y++) {
+                if (chip8_screen_is_set(&chip8.screen, x, y)) {
+                    SDL_Rect r;
+                    r.x = x * CHIP8_WINDOW_MULTIPLIER;
+                    r.y = y * CHIP8_WINDOW_MULTIPLIER;
+                    r.w = CHIP8_WINDOW_MULTIPLIER;
+                    r.h = CHIP8_WINDOW_MULTIPLIER;
+                    SDL_RenderFillRect(renderer, &r);
+                }
+            }
+        }
+
         SDL_RenderPresent(renderer);
     }
     out:
